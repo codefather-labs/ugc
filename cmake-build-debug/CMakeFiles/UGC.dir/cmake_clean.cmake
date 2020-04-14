84af7b5af19df4d1533fff98e2d25f3e405dc338
codefather <codefather.ru@gmail.com>
2020-04-14 21:06:43 +0300

    migrate from local

---
+++ cmake-build-debug/CMakeFiles/UGC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UGC.dir/main.c.o"
+  "UGC"
+  "UGC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/UGC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
